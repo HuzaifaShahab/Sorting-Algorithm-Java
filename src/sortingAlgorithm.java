03de2a7518c2bf253792da942dc64e69e88b7e3e
HUZAIFA SHAHAB <huzaifarifatullah@gmail.com>
2023-07-12 11:55:49 +0500

    Sorting algorithm in java.

---
+++ src/sortingAlgorithm.java
@@ -0,0 +1,27 @@
+public class sortingAlgorithm {
+    public static void main(String[] args) {
+
+        System.out.println("Array Sorting Algorithm Concept Catching !");
+
+        int []array = {9,8,7,4,3,3,2,1} ;   // Initialized An Array.
+
+        int startLoop , minValue , minIndex ;    // Declared Variables.
+        for (startLoop = 0 ; startLoop < array.length-1 ; startLoop++) { // Outer-Loop.
+            minIndex = startLoop ;
+            minValue = array[minIndex] ;
+
+            for (int index = startLoop + 1 ; index < array.length ; index++) { // Inner-Loop.
+                if (array[index] < minValue){
+                    minValue = array[index] ;
+                    minIndex = index ;
+                }
+            }
+            array[minIndex] = array[startLoop] ; // Putting Values For Sorting An Array.
+            array[startLoop] = minValue ;
+        }
+
+        for (int i = 0 ; i < array.length ; i++){ // You can also use an enhance for-each loop.
+            System.out.print(array[i]+" ");
+        }
+    }
+}
